96de2ceca39629b88a2ae47cf946ac928914574f
Sushmzn9 <sushan.maharjan18@gmail.com>
2023-12-15 23:41:19 +1100

    Navbar added to the page

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,6 +1,44 @@
-export const PRODUCT_CATEGORIES: [
+export const PRODUCT_CATEGORIES = [
   {
-    label: "UI Kits";
-    value: "ui_kits" as const;
-  }
+    label: "UI Kits",
+    value: "ui_kits" as const,
+    featured: [
+      {
+        name: "Editor Picks",
+        href: "#",
+        imageSrc: "/nav/ui-kits/mixed.jpg",
+      },
+      {
+        name: "New Arrivals",
+        href: "#",
+        imageSrc: "/nav/ui-kits/blue.jpg",
+      },
+      {
+        name: "Best Sellers",
+        href: "#",
+        imageSrc: "/nav/ui-kits/purple.jpg",
+      },
+    ],
+  },
+  {
+    label: "Icons",
+    value: "icons" as const,
+    featured: [
+      {
+        name: "Favourite Icon",
+        href: "#",
+        imageSrc: "/nav/icons/picks.jpg",
+      },
+      {
+        name: "New Arrivals",
+        href: "#",
+        imageSrc: "/nav/icons/new.jpg",
+      },
+      {
+        name: "Bestselling Icons",
+        href: "#",
+        imageSrc: "/nav/icons/bestsellers.jpg",
+      },
+    ],
+  },
 ];
